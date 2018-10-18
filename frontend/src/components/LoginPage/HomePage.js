08c6b8831825d702b275a564421906fdd9678be3
Mark Jardenil <jardenilmark@gmail.com>
2018-10-18 22:02:41 +0800

    feat: add login home page

--- frontend/src/components/LoginPage/HomePage.js
+++ frontend/src/components/LoginPage/HomePage.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Grid, Button } from 'semantic-ui-react'
+import { Grid, Button, Header } from 'semantic-ui-react'
 
 const loginPage = props => {
 	return (
-		<Grid columns="equal">
-			<Grid.Row>
-				<Grid.Column>
-					<Button content="Teacher" />
+		<Grid
+			columns="equal"
+			style={{ height: '100vh', backgroundColor: '#2a474b', margin: 0 }}
+		>
+			<Grid.Row style={{ width: '100vh' }}>
+				<Grid.Column textAlign="right" verticalAlign="middle">
+					<Button
+						content="Teacher"
+						style={{ margin: 0, padding: 90, fontSize: 50 }}
+					/>
 				</Grid.Column>
-				<Grid.Column>
-					<Button content="Student" />
+				<Grid.Column textAlign="center" style={{ marginTop: 150 }}>
+					<Header style={{ fontSize: 100 }} textAlign="center">
+						LOGIN
+					</Header>
+				</Grid.Column>
+				<Grid.Column textAlign="left" verticalAlign="middle">
+					<Button
+						content="Student"
+						style={{ margin: 0, padding: 90, fontSize: 50 }}
+					/>
 				</Grid.Column>
 			</Grid.Row>
 		</Grid>
