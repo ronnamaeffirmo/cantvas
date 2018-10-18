c49296518934931e83ac75e9b22767761c8346c6
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-18 22:16:08 +0800

    chore: mod eslintrc, rename loginpage

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import StudentPage from './components/StudentPage/HomePage'
-import loginPage from './components/LoginPage/LoginPage'
+import LoginPage from './components/LoginPage/LoginPage'
 
 const Routes = props => {
 	return (
 		<Switch>
-			<Route path="/login" exact component={loginPage} />
+			<Route path="/login" exact component={LoginPage} />
 			<Route path="/student" component={StudentPage} />
 		</Switch>
 	)
