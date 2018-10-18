604abbd4fb6484aac67868c51e8fb741a8e3cbd3
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-19 00:26:33 +0800

    feat: query resolver for new data models

--- server/src/resolvers/query.js
+++ server/src/resolvers/query.js
@@ -5,6 +5,21 @@ module.exports = {
 	students(root, args, context, info) {
 		return context.db.query.students({}, info)
 	},
+	scores(root, args, context, info) {
+		return context.db.query.scores({}, info)
+	},
+	subjects(root, args, context, info) {
+		return context.db.query.subjects({}, info)
+	},
+	exams(root, args, context, info) {
+		return context.db.query.exams({}, info)
+	},
+	questions(root, args, context, info) {
+		return context.db.query.questions({}, info)
+	},
+	choices(root, args, context, info) {
+		return context.db.query.choices({}, info)
+	},
 	teacher(root, args, context, info) {
 		const { where } = args
 		return context.db.query.teacher({ where }, info)
@@ -12,5 +27,25 @@ module.exports = {
 	student(root, args, context, info) {
 		const { where } = args
 		return context.db.query.student({ where }, info)
+	},
+	score(root, args, context, info) {
+		const { where } = args
+		return context.db.query.score({ where }, info)
+	},
+	subject(root, args, context, info) {
+		const { where } = args
+		return context.db.query.subject({ where }, info)
+	},
+	exam(root, args, context, info) {
+		const { where } = args
+		return context.db.query.exam({ where }, info)
+	},
+	question(root, args, context, info) {
+		const { where } = args
+		return context.db.query.question({ where }, info)
+	},
+	choice(root, args, context, info) {
+		const { where } = args
+		return context.db.query.choice({ where }, info)
 	}
 }
