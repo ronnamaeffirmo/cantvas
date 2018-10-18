cbdb0e6d2d9a44c6d0ccbc0758440d1918edcfbc
Mark Jardenil <jardenilmark@gmail.com>
2018-10-18 21:00:59 +0800

    feat: add routes

---
+++ frontend/src/components/LoginPage/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Grid, Button } from 'semantic-ui-react'
+
+const loginPage = props => {
+	return (
+		<Grid columns="equal">
+			<Grid.Row>
+				<Grid.Column>
+					<Button content="Teacher" />
+				</Grid.Column>
+				<Grid.Column>
+					<Button content="Student" />
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	)
+}
+
+export default loginPage
---
+++ frontend/src/components/StudentPage/HomePage.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Menu, Image, Grid, Icon } from 'semantic-ui-react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import Dashboard from './Dashboard'
+const GET_ACTIVE_ITEM = gql`
+	{
+		activeItem @client
+	}
+`
+
+const getBackgroundColor = (name, activeItem) => {
+	if (name === activeItem) {
+		return 'white'
+	}
+	return '#2a474b'
+}
+
+const getFontColor = (name, activeItem) => {
+	if (name === activeItem) {
+		return '#313a96'
+	}
+	return 'white'
+}
+
+const home = props => {
+	return (
+		<Switch>
+			<Query query={GET_ACTIVE_ITEM}>
+				{({ data, client }) => {
+					return (
+						<Grid style={{ padding: 0, margin: 0 }}>
+							<Grid.Column width={1} style={{ padding: 0, margin: 0 }}>
+								<Menu
+									pointing
+									secondary
+									vertical
+									icon="labeled"
+									style={{
+										backgroundColor: '#2a474b',
+										height: '100vh',
+										width: '100%'
+									}}
+								>
+									<Menu.Item disabled>
+										<Image src={require('../../images/logo.png')} />
+									</Menu.Item>
+									<Menu.Item
+										active={data.activeItem === 'dashboard'}
+										style={{
+											borderRight: 0,
+											color: getFontColor('dashboard', data.activeItem),
+											backgroundColor: getBackgroundColor(
+												'dashboard',
+												data.activeItem
+											)
+										}}
+										as={Link}
+										to={{
+											pathname: '/student/dashboard'
+										}}
+										onClick={() =>
+											client.writeData({ data: { activeItem: 'dashboard' } })
+										}
+									>
+										<Icon name="dashboard" />
+										Dashboard
+									</Menu.Item>
+									<Menu.Item
+										active={data.activeItem === 'account'}
+										style={{
+											borderRight: 0,
+											color: getFontColor('account', data.activeItem),
+											backgroundColor: getBackgroundColor(
+												'account',
+												data.activeItem
+											)
+										}}
+										as={Link}
+										to={{
+											pathname: '/student/account'
+										}}
+										onClick={() =>
+											client.writeData({ data: { activeItem: 'account' } })
+										}
+									>
+										<Icon name="user circle outline" />
+										Account
+									</Menu.Item>
+								</Menu>
+							</Grid.Column>
+							<Grid.Column
+								width={12}
+								style={{ padding: 0, margin: 0, paddingLeft: 20 }}
+							>
+								<Route exact path="/student/dashboard" component={Dashboard} />
+							</Grid.Column>
+						</Grid>
+					)
+				}}
+			</Query>
+		</Switch>
+	)
+}
+
+export default home
