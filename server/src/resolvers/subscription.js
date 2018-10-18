d6e64699b4b1d36b5567bc0b6cdfb45da6750934
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-19 00:27:01 +0800

    feat: subscription resolver for new dat amodels

--- server/src/resolvers/subscription.js
+++ server/src/resolvers/subscription.js
@@ -8,8 +8,38 @@ const studentSubscription = {
 		return ctx.db.subscription.student({}, info)
 	}
 }
+const scoreSubscription = {
+	subscribe: (root, args, ctx, info) => {
+		return ctx.db.subscription.score({}, info)
+	}
+}
+const subjectSubscription = {
+	subscribe: (root, args, ctx, info) => {
+		return ctx.db.subscription.subject({}, info)
+	}
+}
+const examSubscription = {
+	subscribe: (root, args, ctx, info) => {
+		return ctx.db.subscription.exam({}, info)
+	}
+}
+const questionSubscription = {
+	subscribe: (root, args, ctx, info) => {
+		return ctx.db.subscription.question({}, info)
+	}
+}
+const choiceSubscription = {
+	subscribe: (root, args, ctx, info) => {
+		return ctx.db.subscription.choice({}, info)
+	}
+}
 
 module.exports = {
 	teacherSubscription,
-	studentSubscription
+	studentSubscription,
+	scoreSubscription,
+	subjectSubscription,
+	examSubscription,
+	questionSubscription,
+	choiceSubscription
 }
