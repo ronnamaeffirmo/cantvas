78e81a3427a05f9779fe992f32a1197096806e17
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-19 00:11:38 +0800

    feat: mutations for new data models

--- server/src/resolvers/mutation.js
+++ server/src/resolvers/mutation.js
@@ -15,6 +15,46 @@ module.exports = {
 			info
 		)
 	},
+	createScore(root, args, context, info) {
+		return context.db.mutation.createScore(
+			{
+				data: args.data
+			},
+			info
+		)
+	},
+	createSubject(root, args, context, info) {
+		return context.db.mutation.createSubject(
+			{
+				data: args.data
+			},
+			info
+		)
+	},
+	createExam(root, args, context, info) {
+		return context.db.mutation.createExam(
+			{
+				data: args.data
+			},
+			info
+		)
+	},
+	createQuestion(root, args, context, info) {
+		return context.db.mutation.createQuestion(
+			{
+				data: args.data
+			},
+			info
+		)
+	},
+	createChoice(root, args, context, info) {
+		return context.db.mutation.createChoice(
+			{
+				data: args.data
+			},
+			info
+		)
+	},
 	updateTeacher(root, args, context, info) {
 		return context.db.mutation.updateTeacher(
 			{
@@ -33,6 +73,51 @@ module.exports = {
 			info
 		)
 	},
+	updateScore(root, args, context, info) {
+		return context.db.mutation.updateScore(
+			{
+				data: args.data,
+				where: args.where
+			},
+			info
+		)
+	},
+	updateSubject(root, args, context, info) {
+		return context.db.mutation.updateSubject(
+			{
+				data: args.data,
+				where: args.where
+			},
+			info
+		)
+	},
+	updateExam(root, args, context, info) {
+		return context.db.mutation.updateExam(
+			{
+				data: args.data,
+				where: args.where
+			},
+			info
+		)
+	},
+	updateQuestion(root, args, context, info) {
+		return context.db.mutation.updateQuestion(
+			{
+				data: args.data,
+				where: args.where
+			},
+			info
+		)
+	},
+	updateChoice(root, args, context, info) {
+		return context.db.mutation.updateChoice(
+			{
+				data: args.data,
+				where: args.where
+			},
+			info
+		)
+	},
 	deleteTeacher(root, args, context, info) {
 		return context.db.mutation.deleteTeacher({
 			where: args.where
@@ -40,6 +125,31 @@ module.exports = {
 	},
 	deleteStudent(root, args, context, info) {
 		return context.db.mutation.deleteStudent({
+			where: args.where
+		})
+	},
+	deleteScore(root, args, context, info) {
+		return context.db.mutation.deleteScore({
+			where: args.where
+		})
+	},
+	deleteSubject(root, args, context, info) {
+		return context.db.mutation.deleteSubject({
+			where: args.where
+		})
+	},
+	deleteExam(root, args, context, info) {
+		return context.db.mutation.deleteExam({
+			where: args.where
+		})
+	},
+	deleteQuestion(root, args, context, info) {
+		return context.db.mutation.deleteQuestion({
+			where: args.where
+		})
+	},
+	deleteChoice(root, args, context, info) {
+		return context.db.mutation.deleteChoice({
 			where: args.where
 		})
 	}
