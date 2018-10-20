2432f599bc804a03721ab04a289c8aa8ed565907
Mark Jardenil <jardenilmark@gmail.com>
2018-10-20 18:43:11 +0800

    refactor: separate components

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,8 +3,8 @@ import { Switch, Route } from 'react-router-dom'
 import StudentPage from './components/StudentPage/HomePage'
 import teacherPage from './components/TeacherPage/HomePage'
 import HomePage from './components/LoginPage/HomePage'
-import studentRegisterPage from './components/LoginPage/StudentRegisterPage'
-import teacherRegisterPage from './components/LoginPage/TeacherRegisterPage'
+import studentRegisterPage from './components/RegisterPage/StudentRegisterPage'
+import teacherRegisterPage from './components/RegisterPage/TeacherRegisterPage'
 import studentLoginPage from './components/LoginPage/StudentLoginPage'
 import teacherLoginPage from './components/LoginPage/TeacherLoginPage'
 
@@ -14,10 +14,10 @@ const Routes = props => {
 			<Route path={'/home'} exact component={HomePage} />
 			<Route path={'/student'} component={StudentPage} />
 			<Route path={'/teacher'} component={teacherPage} />
-			<Route path={'/studentLogin'} exact component={studentLoginPage} />
-			<Route path={'/teacherLogin'} exact component={teacherLoginPage} />
-			<Route path={'/studentRegister'} exact component={studentRegisterPage} />
-			<Route path={'/teacherRegister'} exact component={teacherRegisterPage} />
+			<Route path={'/student-login'} exact component={studentLoginPage} />
+			<Route path={'/teacher-login'} exact component={teacherLoginPage} />
+			<Route path={'/student-register'} exact component={studentRegisterPage} />
+			<Route path={'/teacher-register'} exact component={teacherRegisterPage} />
 		</Switch>
 	)
 }
---
+++ frontend/src/components/RegisterPage/StudentRegisterPage.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Container, Header, Grid, Image } from 'semantic-ui-react'
+import StudentRegisterForm from './StudentRegisterForm'
+
+const registerPage = props => {
+	return (
+		<Container fluid style={style.container}>
+			<Container style={style.header}>
+				<Grid style={style.grid}>
+					<Grid.Row>
+						<Grid.Column style={style.gridColumn} width={1}>
+							<Image src={require('../../images/logo.png')} style={style.image} />
+						</Grid.Column>
+						<Grid.Column width={15} verticalAlign={'middle'}>
+							<Header size={'huge'}>Registration Page</Header>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Container>
+			<Container textAlign={'center'} style={style.form}>
+				<StudentRegisterForm />
+			</Container>
+			<Container style={style.footer} />
+		</Container>
+	)
+}
+
+const style = {
+	footer: {
+		height: '10vh',
+		width: '100%',
+		backgroundColor: 'black',
+		position: 'absolute',
+		bottom: 0
+	},
+	form: {
+		backgroundColor: 'white',
+		width: '30%',
+		marginTop: 20
+	},
+	container: {
+		height: '100vh',
+		width: '100%',
+		backgroundColor: '#2a474b'
+	},
+	header: {
+		height: '10vh',
+		width: '100%',
+		backgroundColor: 'white'
+	},
+	grid: {
+		height: '100%',
+		width: '100%'
+	},
+	gridColumn: {
+		height: '100%'
+	},
+	image: {
+		height: '100%',
+		marginLeft: 20,
+		marginTop: 5
+	}
+}
+
+export default registerPage
