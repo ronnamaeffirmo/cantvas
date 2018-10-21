f5f239600970c2956b4f6ee39d70aaf0b76d82d6
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-21 23:39:57 +0800

    style: custom item

--- frontend/src/components/CustomComponents/CustomInput.js
+++ frontend/src/components/CustomComponents/CustomInput.js
@@ -3,10 +3,19 @@ import { Form } from 'semantic-ui-react'
 
 const CustomInput = ({ input, meta, ...formInput }) => (
 	<Fragment>
-		<Form.Input {...input} {...formInput} fluid iconPosition={'left'} />
-		{meta.error &&
-			meta.touched && <p style={{ color: 'red', textAlign: 'right' }}>*{meta.error}</p>}
+		{meta.error && meta.touched && <p style={style.error}>*{meta.error}</p>}
+		<Form.Input style={style.input} {...input} {...formInput} fluid iconPosition={'left'} />
 	</Fragment>
 )
+
+const style = {
+	error: {
+		color: 'red',
+		textAlign: 'right',
+		paddingBottom: 0,
+		marginBottom: 0,
+		fontSize: '12px'
+	}
+}
 
 export default CustomInput
