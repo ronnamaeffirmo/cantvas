cdae35e87b0151398d575e5272181f4f6aac5ba1
Mark Jardenil <jardenilmark@gmail.com>
2018-10-21 13:25:15 +0800

    refactor: change login button to submit

--- frontend/src/components/TeacherPage/CreateExam.js
+++ frontend/src/components/TeacherPage/CreateExam.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Header, Divider } from 'semantic-ui-react'
 
 const CreateExam = props => {
-	return <div>test</div>
+	return (
+		<div>
+			<Header size={'huge'} style={style.header}>
+				Create Exam
+			</Header>
+			<Divider />
+		</div>
+	)
 }
 
+const style = {
+	header: {
+		fontWeight: 'normal'
+	}
+}
 export default CreateExam
